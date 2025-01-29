686207912e49bfa85fa11c7aab785f53464e8bbc
Robinson Lopez <robins.616009@gmail.com>
2025-01-29 18:24:06 -0500

    Final app

--- src/servicio/ServicioPeliculasArchivo.java
+++ src/servicio/ServicioPeliculasArchivo.java
@@ -48,11 +48,50 @@ public class ServicioPeliculasArchivo implements IServicioPeliculas{
 
     @Override
     public void agregarPelicula(Pelicula pelicula) {
-
+        boolean anexar = false;
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{ //Si ya existe el archivo, No se vuelva a crear
+            System.out.println("Listado de Peliculas");
+            anexar = archivo.exists();
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            // Agregamos la pelicula (toString)
+            salida.println(pelicula);
+            salida.close();
+            System.out.println("Se agrego al archivo: " + pelicula);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al Agregar pelicula" + e.getMessage());
+        }
     }
 
     @Override
     public void buscarPelicula(Pelicula pelicula) {
-
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{ //Abrimos el archivo para lectura linea a linea
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String lineaTexto;
+            lineaTexto = entrada.readLine();
+            var indice = 1;
+            var encontrada = false;
+            var peliculaBuscar = pelicula.getNombre();
+            while (lineaTexto != null){
+                //Buscamos sin importar mayus/min
+                if (peliculaBuscar != null && peliculaBuscar.equalsIgnoreCase(lineaTexto)){
+                    encontrada = true;
+                    break;
+                }
+                // Leemos la sgte linea antes de la sgte iteracion
+                lineaTexto = entrada.readLine();
+                indice++;
+            } // fin while
+            //imprimimos resultados de la busqueda
+            if (encontrada){
+                System.out.println("Pelicula " + lineaTexto + " encontrada - linea " + indice);
+            } else {
+                System.out.println("No se encontro la pelicula:  " + pelicula.getNombre());
+            }
+            entrada.close();
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al Buscar en elarchivo" + e.getMessage());
+        }
     }
 }
